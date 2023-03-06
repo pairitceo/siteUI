5040e4c0997437c949cf42ce2f4fa3d3992e71de
pairitceo <127158602+pairitceo@users.noreply.github.com>
2023-03-06 18:37:48 -0500

    Initial stack that I previously created

---
+++ index.js
@@ -0,0 +1,23 @@
+function toggleTable1() {
+
+  document.getElementById("creator").classList.toggle("hidden");
+
+}
+
+function toggleTable2() {
+
+    document.getElementById("group").classList.toggle("hidden");
+  
+  }
+
+  function toggleTable3() {
+
+    document.getElementById("trending").classList.toggle("hidden");
+  
+  }
+
+  function toggleTable4() {
+
+    document.getElementById("content").classList.toggle("hidden");
+  
+  }
